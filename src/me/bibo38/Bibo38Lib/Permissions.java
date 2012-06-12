c9f6073e273dee8a4852e574a35abcca43eaef7d
bibo38 <bibo38@github.com>
2012-06-12 18:29:50 +0200

    Added permission error warning

--- src/me/bibo38/Bibo38Lib/Permissions.java
+++ src/me/bibo38/Bibo38Lib/Permissions.java
@@ -1,5 +1,7 @@
 package me.bibo38.Bibo38Lib;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
@@ -51,18 +53,84 @@ public class Permissions
 	 * Funktion zum Überprüfen von Permissions
 	 * für Plugins
 	 * 
+	 * @param player Der zu überprüfende Spieler
+	 * @param permstr Permission Node
+	 * @param showError Soll ein Fehler dem Spieler gezeigt werden?
+	 * @return true falls der Spieler die Permission besitzt
+	 */
+	public boolean hasPerm(Player player, String permstr, boolean showError)
+	{
+		if(player == null) // Falls es die konsole ist!
+		{
+			return true; // Hat Zugriff
+		}
+		
+		boolean erg = false;
+		if(main.vaultOn && !this.vaultError())
+		{
+			erg = perm.has(player, father + permstr);
+		} else
+		{
+			erg = player.hasPermission(father + permstr);
+		}
+		
+		if(!erg && showError) // Falls es nicht erlaubt ist!
+		{
+			player.sendMessage(ChatColor.RED + "You don't have permissions to perform this operation!");
+		}
+		
+		return erg;
+	}
+	
+	/**
+	 * Funktion zum Überprüfen von Permissions
+	 * für Plugins und die Ausgabe einer Fehlermeldung an den Player
+	 * 
 	 * @param player Spieler für den die Permission zutreffen soll
 	 * @param permstr Permission Node
 	 * @return true falls der Spieler die Permission besitzt
 	 */
 	public boolean hasPerm(Player player, String permstr)
 	{
-		if(main.vaultOn && !this.vaultError())
+		return this.hasPerm(player, permstr, true);
+	}
+	
+	/**
+	 * Funktion zum Überprüfen von Permissions
+	 * für Plugins und die Ausgabe einer Fehlermeldung an den Player
+	 * 
+	 * @param cs Der CommandSender, kann eine Konsole sein
+	 * @param permstr Permission Node
+	 * @return true falls der CommandSender Berechtigungen hat
+	 */
+	public boolean hasPerm(CommandSender cs, String permstr)
+	{
+		if(cs instanceof Player)
 		{
-			return perm.has(player, father + permstr);
+			return this.hasPerm((Player) cs, permstr, true);
 		} else
 		{
-			return player.hasPermission(father + permstr);
+			return true;
+		}
+	}
+	
+	/**
+	 * Funktion zum Überprüfen von Permissions
+	 * für Plugins
+	 * 
+	 * @param cs Der CommandSender, kann eine Konsole sein
+	 * @param permstr Permission Node
+	 * @param showError Soll eine Fehlermeldung an den CommandSender gesendet werden?
+	 * @return true falls der CommandSender Berechtigungen hat
+	 */
+	public boolean hasPerm(CommandSender cs, String permstr, boolean showError)
+	{
+		if(cs instanceof Player)
+		{
+			return this.hasPerm((Player) cs, permstr, showError);
+		} else
+		{
+			return true;
 		}
 	}
 }
